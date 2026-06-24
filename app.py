877fe4cb8cd53b2f0d490680a1bc223f486d2667
Lucas <lucas@gmail.com>
2026-06-24 17:26:29 -0300

    Ajustes dia 24-06

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def LOGIN():
+    return render_template('LOGIN.html')
+
+@app.route('/CONTROLE.html')
+def CONTROLE():
+    return render_template('CONTROLE.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
